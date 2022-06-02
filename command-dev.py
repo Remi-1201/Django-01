79937f446cba25ec624adeeee1eae6fc64e997d3
remi-1201 <remi.nkjm@gmail.com>
2022-06-02 17:07:38 +0900

    Make models

--- command-dev.py
+++ command-dev.py
@@ -1,2 +1,31 @@
 # Make new app
 python3 manage.py startapp blog
+
+# config\settings.py
+# Change LANGUAGE_CODE & TIME_ZONE 
+LANGUAGE_CODE = 'ja'
+TIME_ZONE = 'Asia/Tokyo'
+
+# config\settings.py
+# add to INSTALLED_APPS
+'blog.apps.BlogConfig',
+
+# Create models
+# 1- blog\models.py - define the model
+class Post(models.Model): 
+  title = models.CharField(max_length = 100) 
+  content = models.TextField() 
+  # TextField = no need to define max length
+  created_at = models.DateTimeField(auto_now_add = True) 
+  updated_at = models.DateTimeField(auto_now = True)
+  
+  def __str__(self):
+    return self.title
+
+# 2- Make migrations file
+python3 manage.py makemigrations
+# 3- Submit to the database
+python3 manage.py migrate
+
+# Start server
+python3 manage.py runserver
